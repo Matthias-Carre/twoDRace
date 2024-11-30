a89fa47b0e051664b45e660b654b4633bd4a8c5c
Anthony <61050822+JokerOnWeed@users.noreply.github.com>
2024-11-30 15:13:35 +0100

    Update and rename player.js to Player.js

---
+++ js/Player.js
@@ -0,0 +1,13 @@
+Class Player {  
+  constructor(color, life, points, x_axis, y_axis) {
+    this.color = color;
+    this.life = life;
+    this.points = points;
+    this.x_axis = x_axis;
+    this.y_axis = y_axis;
+  }
+}
+
+set_x_axis(posX){this.x_axis = posX;}
+set_y_axis(posY){this.x_axis = posX;}
+};
