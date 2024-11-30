534775f41fe53b65eecb65b039eaeecb3d3b2d91
Anthony <61050822+JokerOnWeed@users.noreply.github.com>
2024-11-30 15:15:17 +0100

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,13 +1,14 @@
 Class Player {  
-  constructor(color, life, points, x_axis, y_axis) {
+  constructor(color, life, points, speed, x_axis, y_axis) {
     this.color = color;
     this.life = life;
     this.points = points;
+    this.speed = speed;
     this.x_axis = x_axis;
     this.y_axis = y_axis;
   }
 }
 
 set_x_axis(posX){this.x_axis = posX;}
-set_y_axis(posY){this.x_axis = posX;}
+set_y_axis(posY){this.y_axis = posX;}
 };
