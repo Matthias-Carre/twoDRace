a38db34790e40006444ba5323ba16f921bfb9554
Anthony <61050822+JokerOnWeed@users.noreply.github.com>
2024-12-12 11:05:14 +0100

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,12 +1,22 @@
-export class Player {  
-  constructor(color, life, points, speed, x_axis, y_axis){
+export default class Player {  
+  constructor(color, x_axis, y_axis){
     this.color = color;
-    this.life = life;
-    this.points = points;
-    this.speed = speed;
+    //this.life = life;
+    //this.points = points;
+    //this.speed = speed;
     this.x_axis = x_axis;
     this.y_axis = y_axis;
   }
-set_x_axis(posX){this.x_axis = posX;}
-set_y_axis(posY){this.y_axis = posY;}
+  
+  set_x_axis(posX){this.x_axis = posX;}
+  set_y_axis(posY){this.y_axis = posY;}
+  
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillRect(this.x_axis, this.y_axis, 100, 100);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+    console.log('draw');
+  }
 };
